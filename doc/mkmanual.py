d9a95779be0aee2877dc70ac5459726aecf94d16
Joshua Kas <Joshua Kas>
2026-09-21 23:19:45 -0700

    Added to documentation.

--- doc/mkmanual.py
+++ doc/mkmanual.py
@@ -678,6 +678,9 @@ Supported schema types:
 \section{The minimal input, and most important keywords}
 \input{minimal_input}
 
+\section{The corvus GUI (SciGUI)}
+\input{scigui}
+
 \section{Output directory structure and output files}
 \input{output_files.tex}
 
